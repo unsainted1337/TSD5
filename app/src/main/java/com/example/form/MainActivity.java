d9db18f02b7e477264d468ebe8344f1519a31a35
Nikita <nikitatr99@yandex.ru>
2020-11-08 23:24:06 +0400

    changed form style

--- app/src/main/java/com/example/form/MainActivity.java
+++ app/src/main/java/com/example/form/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.form;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
-//https://open.spotify.com/track/0tyR7Bu9P086aWBFZ4QJoo?si=wBQAm4MERACmdfljDJ5_bw
+
+    TextInputEditText title_input;
+
+    TextInputEditText sub_title_input;
+
+    TextInputEditText author_input;
+
+    Spinner type_spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title_input = findViewById(R.id.title);
+
+        sub_title_input = findViewById(R.id.sub_title);
+
+        author_input = findViewById(R.id.author);
+
+        type_spinner = findViewById(R.id.spinner);
+        String type1 = String.valueOf(type_spinner.getSelectedItemPosition()); // Хуйня. Значение возмётся 1 раз при запуске активити, а там по умолчанию будет 0. Нужен onSelectedItemListener()
     }
-    EditText title1 = (EditText) findViewById(R.id.title1);
-    EditText subtitle1 = (EditText) findViewById(R.id.subtitle1);
-    EditText writer1 = (EditText) findViewById(R.id.writer1);
-    Spinner spinner = (Spinner) findViewById(R.id.spinner);
-    String type1 = String.valueOf(spinner.getSelectedItemPosition());
 }
