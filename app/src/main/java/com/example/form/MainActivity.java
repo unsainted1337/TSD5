a1853e5718b3bad5a91d2a9a10fef5d6f44a8bba
Дмитрий <bushuev.0221@gmail.com>
2020-11-08 22:50:57 +0400

    gg

--- app/src/main/java/com/example/form/MainActivity.java
+++ app/src/main/java/com/example/form/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 //https://open.spotify.com/track/0tyR7Bu9P086aWBFZ4QJoo?si=wBQAm4MERACmdfljDJ5_bw
@@ -12,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    EditText title1 = (EditText) findViewById(R.id.title1);
+    EditText subtitle1 = (EditText) findViewById(R.id.subtitle1);
+    EditText writer1 = (EditText) findViewById(R.id.writer1);
+    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+    String type1 = String.valueOf(spinner.getSelectedItemPosition());
 }
