855e46e2f97e7b10e11dfa1ab5baabe4091f6884
Nikita <Nikitatr99@yandex.ru>
2020-11-08 22:15:46 +0400

    change comment

--- app/src/main/java/com/example/form/MainActivity.java
+++ app/src/main/java/com/example/form/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-//ggwp
+//https://open.spotify.com/track/0tyR7Bu9P086aWBFZ4QJoo?si=wBQAm4MERACmdfljDJ5_bw
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
